c2cc34ad3c2806a3baba46300b81203babda2506
Taylor Whatley <32211852+1whatleytay@users.noreply.github.com>
2023-07-22 22:52:28 -0700

    Allow serialization for Instructions

--- src/unit/instruction.rs
+++ src/unit/instruction.rs
@@ -1,6 +1,7 @@
 use crate::cpu::decoder::Decoder;
 use crate::unit::register::RegisterName;
 use num::FromPrimitive;
+use num_traits::Signed;
 
 #[allow(dead_code)]
 #[derive(Clone, Debug)]
@@ -66,6 +67,18 @@ pub enum Instruction {
     Mtlo { s: RegisterName },
     Trap,
     Syscall,
+}
+
+fn sig(imm: u16) -> String {
+    let value = imm as i16 as i64;
+
+    if value.abs() < 10 {
+        format!("{value}")
+    } else {
+        let sign = if value < 0 { "-" } else { "" };
+
+        format!("{}0x{:x}", sign, value.abs())
+    }
 }
 
 fn jump_dest(pc: u32, imm: u32) -> u32 {
@@ -335,5 +348,73 @@ impl Decoder<Instruction> for InstructionDecoder {
 
     fn syscall(&mut self) -> Instruction {
         Instruction::Syscall
+    }
+}
+
+impl ToString for Instruction {
+    fn to_string(&self) -> String {
+        match self {
+            Instruction::Add { s, t, d } => format!("add {}, {}, {}", d, s, t),
+            Instruction::Addu { s, t, d } => format!("addu {}, {}, {}", d, s, t),
+            Instruction::And { s, t, d } => format!("and {}, {}, {}", d, s, t),
+            Instruction::Div { s, t } => format!("div {}, {}", s, t),
+            Instruction::Divu { s, t } => format!("divu {}, {}", s, t),
+            Instruction::Mult { s, t } => format!("mult {}, {}", s, t),
+            Instruction::Multu { s, t } => format!("multu {}, {}", s, t),
+            Instruction::Nor { s, t, d } => format!("nor {}, {}, {}", d, s, t),
+            Instruction::Or { s, t, d } => format!("or {}, {}, {}", d, s, t),
+            Instruction::Sll { t, d, sham } => format!("sll {}, {}, {}", d, t, sham),
+            Instruction::Sllv { s, t, d } => format!("sllv {}, {}, {}", d, t, s),
+            Instruction::Sra { t, d, sham } => format!("sra {}, {}, {}", d, t, sham),
+            Instruction::Srav { s, t, d } => format!("srav {}, {}, {}", d, t, s),
+            Instruction::Srl { t, d, sham } => format!("srl {}, {}, {}", d, t, sham),
+            Instruction::Srlv { s, t, d } => format!("srlv {}, {}, {}", d, t, s),
+            Instruction::Sub { s, t, d } => format!("sub {}, {}, {}", s, t, d),
+            Instruction::Subu { s, t, d } => format!("subu {}, {}, {}", s, t, d),
+            Instruction::Xor { s, t, d } => format!("xor {}, {}, {}", s, t, d),
+            Instruction::Slt { s, t, d } => format!("slt {}, {}, {}", s, t, d),
+            Instruction::Sltu { s, t, d } => format!("sltu {}, {}, {}", s, t, d),
+            Instruction::Jr { s } => format!("jr {}", s),
+            Instruction::Jalr { s } => format!("jalr {}", s),
+            Instruction::Madd { s, t } => format!("madd {}, {}", s, t),
+            Instruction::Maddu { s, t } => format!("maddu {}, {}", s, t),
+            Instruction::Mul { s, t, d } => format!("mul {}, {}, {}", d, s, t),
+            Instruction::Msub { s, t } => format!("msub {}, {}", s, t),
+            Instruction::Msubu { s, t } => format!("msubu {}, {}", s, t),
+            Instruction::Addi { s, t, imm } => format!("addi {}, {}, {}", t, s, sig(*imm)),
+            Instruction::Addiu { s, t, imm } => format!("addiu {}, {}, {}", t, s, sig(*imm)),
+            Instruction::Andi { s, t, imm } => format!("andi {}, {}, {}", t, s, sig(*imm)),
+            Instruction::Ori { s, t, imm } => format!("ori {}, {}, {}", t, s, sig(*imm)),
+            Instruction::Xori { s, t, imm } => format!("xori {}, {}, {}", t, s, sig(*imm)),
+            Instruction::Lui { s, imm } => format!("lui {}, {}", s, sig(*imm)),
+            Instruction::Lhi { t, imm } => format!("lhi {}, {}", t, sig(*imm)),
+            Instruction::Llo { t, imm } => format!("llo {}, {}", t, sig(*imm)),
+            Instruction::Slti { s, t, imm } => format!("slti {}, {}, {}", t, s, sig(*imm)),
+            Instruction::Sltiu { s, t, imm } => format!("sltiu {}, {}, {}", t, s, sig(*imm)),
+            Instruction::Beq { s, t, address } => format!("beq {}, {}, 0x{:x}", s, t, address),
+            Instruction::Bne { s, t, address } => format!("bne {}, {}, 0x{:x}", s, t, address),
+            Instruction::Bgtz { s, address } => format!("bgtz {}, 0x{:x}", s, address),
+            Instruction::Blez { s, address } => format!("blez {}, 0x{:x}", s, address),
+            Instruction::Bltz { s, address } => format!("bltz {}, 0x{:x}", s, address),
+            Instruction::Bgez { s, address } => format!("bgez {}, 0x{:x}", s, address),
+            Instruction::Bltzal { s, address } => format!("bltzal {}, 0x{:x}", s, address),
+            Instruction::Bgezal { s, address } => format!("bgezal {}, 0x{:x}", s, address),
+            Instruction::J { address } => format!("j 0x{:x}", address),
+            Instruction::Jal { address } => format!("jal 0x{:x}", address),
+            Instruction::Lb { s, t, imm } => format!("lb {}, {}, {}", s, t, sig(*imm)),
+            Instruction::Lbu { s, t, imm } => format!("lbu {}, {}, {}", s, t, sig(*imm)),
+            Instruction::Lh { s, t, imm } => format!("lh {}, {}, {}", s, t, sig(*imm)),
+            Instruction::Lhu { s, t, imm } => format!("lhu {}, {}, {}", s, t, sig(*imm)),
+            Instruction::Lw { s, t, imm } => format!("lw {}, {}, {}", s, t, sig(*imm)),
+            Instruction::Sb { s, t, imm } => format!("sb {}, {}, {}", s, t, sig(*imm)),
+            Instruction::Sh { s, t, imm } => format!("sh {}, {}, {}", s, t, sig(*imm)),
+            Instruction::Sw { s, t, imm } => format!("sw {}, {}, {}", s, t, sig(*imm)),
+            Instruction::Mfhi { d } => format!("mfhi {}", d),
+            Instruction::Mflo { d } => format!("mflo {}", d),
+            Instruction::Mthi { s } => format!("mthi {}", s),
+            Instruction::Mtlo { s } => format!("mtlo {}", s),
+            Instruction::Trap => "trap".to_string(),
+            Instruction::Syscall => "syscall".to_string(),
+        }
     }
 }
--- src/unit/register.rs
+++ src/unit/register.rs
@@ -1,3 +1,4 @@
+use std::fmt::{Display, Formatter};
 use crate::cpu::state::Registers;
 use num_derive::{ToPrimitive, FromPrimitive};
 use num_traits::ToPrimitive;
@@ -10,6 +11,51 @@ pub enum RegisterName {
     S0 = 16, S1 = 17, S2 = 18, S3 = 19, S4 = 20, S5 = 21, S6 = 22, S7 = 23,
     T8 = 24, T9 = 25, K0 = 26, K1 = 27,
     GP = 28, SP = 29, FP = 30, RA = 31,
+}
+
+impl ToString for RegisterName {
+    fn to_string(&self) -> String {
+        match self {
+            RegisterName::Zero => "zero".to_string(),
+            RegisterName::AT => "at".to_string(),
+            RegisterName::V0 => "v0".to_string(),
+            RegisterName::V1 => "v1".to_string(),
+            RegisterName::A0 => "a0".to_string(),
+            RegisterName::A1 => "a1".to_string(),
+            RegisterName::A2 => "a2".to_string(),
+            RegisterName::A3 => "a3".to_string(),
+            RegisterName::T0 => "t0".to_string(),
+            RegisterName::T1 => "t1".to_string(),
+            RegisterName::T2 => "t2".to_string(),
+            RegisterName::T3 => "t3".to_string(),
+            RegisterName::T4 => "t4".to_string(),
+            RegisterName::T5 => "t5".to_string(),
+            RegisterName::T6 => "t6".to_string(),
+            RegisterName::T7 => "t7".to_string(),
+            RegisterName::S0 => "s0".to_string(),
+            RegisterName::S1 => "s1".to_string(),
+            RegisterName::S2 => "s2".to_string(),
+            RegisterName::S3 => "s3".to_string(),
+            RegisterName::S4 => "s4".to_string(),
+            RegisterName::S5 => "s5".to_string(),
+            RegisterName::S6 => "s6".to_string(),
+            RegisterName::S7 => "s7".to_string(),
+            RegisterName::T8 => "t8".to_string(),
+            RegisterName::T9 => "t9".to_string(),
+            RegisterName::K0 => "k0".to_string(),
+            RegisterName::K1 => "k1".to_string(),
+            RegisterName::GP => "gp".to_string(),
+            RegisterName::SP => "sp".to_string(),
+            RegisterName::FP => "fp".to_string(),
+            RegisterName::RA => "ra".to_string(),
+        }
+    }
+}
+
+impl Display for RegisterName {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "${}", self.to_string())
+    }
 }
 
 impl Registers {
