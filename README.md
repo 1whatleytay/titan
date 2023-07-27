d3bc2c3392c8a8446ef53d39048f5a4ef74549f9
Taylor Whatley <32211852+1whatleytay@users.noreply.github.com>
2023-07-27 00:12:51 -0700

    Add README.md

---
+++ README.md
@@ -0,0 +1,26 @@
+# Titan - MIPS Backend
+
+Titan provides a MIPS assembler, interpret and debugger.
+
+It is the backend for the [Saturn](https://github.com/1whatleytay/saturn) Modern MIPS IDE.
+
+# Project Goals
+
+- **Performance and Stability** - A fast environment that can keep up with complex programs.
+- **Cross Platform** - Tools should be able to be built and run on any platform, with no complications.
+- **Powerful Debugging and Testing** - Write your own tests for assembly that can debug and access state.
+
+# Building
+
+Titan is built with Rust. You can download rust from [Rust](https://www.rust-lang.org).
+
+To build the library, use
+```
+cargo build
+```
+
+There is currently no main target, except for the titan-cli submodule.
+To run it, inside the src/titan-cli, use:
+```
+cargo run -- build path/to/file.asm
+```
--- src/cpu/error.rs
+++ src/cpu/error.rs
@@ -13,15 +13,15 @@ impl Display for Error {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
             Error::MemoryAlign(address) => {
-                write!(f, "Invalid memory alignment for address 0x{address:08x}")
+                write!(f, "Memory access for address 0x{address:08x} is prohibited (not aligned for this instruction, ensure its a multiple of 2 or 4).")
             }
             Error::MemoryUnmapped(address) => {
-                write!(f, "Unmapped memory region for address 0x{address:08x}")
+                write!(f, "Memory access for address 0x{address:08x} is prohibited (unmapped memory).")
             }
             Error::CpuInvalid(instruction) => {
                 write!(f, "Invalid CPU instruction 0x{instruction:08x}")
             }
-            Error::CpuTrap => write!(f, "CPU Trap was thrown"),
+            Error::CpuTrap => write!(f, "The instruction was given invalid parameters (CPU Trap was thrown)."),
             Error::CpuSyscall => write!(f, "CPU Syscall was not handled"),
         }
     }
