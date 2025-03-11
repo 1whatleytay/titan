0e54f44a1d83e7edb91dd3b4e70b9a82dcb2136f
Milo <milomg4@gmail.com>
2025-03-11 12:45:25 -0400

    parse floats

--- src/assembler/directive.rs
+++ src/assembler/directive.rs
@@ -2,8 +2,8 @@ use crate::assembler::assembler_util::AssemblerReason::{
     ConstantOutOfRange, EndOfFile, ExpectedConstant, MissingRegion, OverwriteEdge, UnknownDirective,
 };
 use crate::assembler::assembler_util::{
-    default_start, get_constant, get_integer, get_integer_adjacent, get_label, get_string,
-    pc_for_region, AssemblerError,
+    default_start, get_constant, get_float, get_integer, get_integer_adjacent, get_label,
+    get_string, pc_for_region, AssemblerError,
 };
 use crate::assembler::binary::AddressLabel::Label;
 use crate::assembler::binary::BinarySection::{Data, KernelData, KernelText, Text};
@@ -377,12 +377,148 @@ fn do_word_directive(
     Ok(())
 }
 
+struct FloatInfo {
+    value: f32,
+    count: u64,
+}
+
+enum FloatOrLabel {
+    Float(FloatInfo),
+    Label(NamedLabel),
+}
+
+fn grab_float_value(
+    value: &Token,
+    iter: &mut LexerCursor,
+) -> Result<Option<FloatInfo>, AssemblerError> {
+    let Some(value) = get_float(value, iter, true) else {
+        return Ok(None);
+    };
+
+    let next_up = iter.seek_without(is_adjacent_kind);
+
+    let count = if next_up.map(|x| x.kind == Colon).unwrap_or(false) {
+        iter.next();
+
+        let Some(token) = iter.next_adjacent() else {
+            return Err(AssemblerError {
+                location: None,
+                reason: EndOfFile,
+            });
+        };
+
+        let Some(value) = get_integer(token, iter, false) else {
+            return Err(AssemblerError {
+                location: Some(token.location),
+                reason: ExpectedConstant(token.kind.strip()),
+            });
+        };
+
+        if value > REPEAT_LIMIT {
+            return Err(AssemblerError {
+                location: Some(token.location),
+                reason: ConstantOutOfRange(0, REPEAT_LIMIT as i64),
+            });
+        }
+
+        value
+    } else {
+        1u64
+    };
+
+    Ok(Some(FloatInfo { value, count }))
+}
+
+fn get_float_or_labels(iter: &mut LexerCursor) -> Result<Vec<FloatOrLabel>, AssemblerError> {
+    let mut result: Vec<FloatOrLabel> = vec![];
+
+    while let Some(value) = iter.seek_without(is_solid_kind) {
+        let start = iter.get_position();
+
+        let item = if let TokenKind::Symbol(name) = &value.kind {
+            // This is workaroundy, but a symbol can also be a label
+
+            iter.next();
+
+            let (_, token) = iter.peek_adjacent();
+
+            let do_skip = match token.map(|x| &x.kind) {
+                Some(Colon) => true,     // label
+                Some(LeftBrace) => true, // Macro
+                _ => false,
+            };
+
+            // This is obviously a sign that the directive section has to be reworked.
+            if do_skip {
+                iter.set_position(start);
+
+                break;
+            }
+
+            let address = NamedLabel {
+                name: name.get().to_string(),
+                location: value.location,
+                offset: 0,
+            };
+
+            FloatOrLabel::Label(address)
+        } else {
+            let Some(constant) = grab_float_value(value, iter)? else {
+                break;
+            };
+
+            FloatOrLabel::Float(constant)
+        };
+
+        result.push(item);
+    }
+    Ok(result)
+}
+
 // Don't want to deal with this until coprocessor
-fn do_float_directive(_: &mut LexerCursor, _: &mut BinaryBuilder) -> Result<(), AssemblerError> {
-    Err(AssemblerError {
-        location: None,
-        reason: UnknownDirective("float".to_string()),
-    })
+fn do_float_directive(
+    iter: &mut LexerCursor,
+    builder: &mut BinaryBuilder,
+) -> Result<(), AssemblerError> {
+    let values = get_float_or_labels(iter)?;
+
+    let region = builder.region().ok_or(MISSING_REGION)?;
+
+    align_with_zeros(region, 4)?;
+
+    for value in values {
+        match value {
+            FloatOrLabel::Label(label) => {
+                let offset = region.raw.data.len();
+
+                region.raw.data.extend_from_slice(&[0u8; 4]);
+                region.labels.push(BinaryBuilderLabel {
+                    offset,
+                    location: label.location,
+                    label: InstructionLabel {
+                        kind: InstructionLabelKind::Full,
+                        label: Label(label),
+                    },
+                })
+            }
+            FloatOrLabel::Float(value) => {
+                if value.count > REPEAT_LIMIT {
+                    continue;
+                }
+
+                let mut array = [0u8; 4];
+                LittleEndian::write_f32(&mut array, value.value);
+
+                region.raw.data.reserve(4 * value.count as usize);
+
+                for _ in 0..value.count {
+                    region.raw.data.extend_from_slice(&array);
+                }
+            }
+        }
+    }
+
+    Ok(())
 }
 
 fn do_double_directive(_: &mut LexerCursor, _: &mut BinaryBuilder) -> Result<(), AssemblerError> {
