735fc2b64bf28edfa2baf4ecd6f1907bba44994a
Taylor Whatley <32211852+1whatleytay@users.noreply.github.com>
2025-07-01 04:31:56 +0200

    Add RISC-V history tracker

---
+++ titan-riscv/src/execution/trackers/history.rs
@@ -0,0 +1,84 @@
+use crate::cpu::memory::watched::{WatchEntry, WatchedMemory, LOG_SIZE};
+use crate::cpu::registers::watched::REGISTER_LOG_SIZE;
+use crate::cpu::registers::{RegisterEntry, Registers, WatchedRegisters, WhichRegister};
+use crate::cpu::{Memory, State};
+use smallvec::SmallVec;
+use std::collections::VecDeque;
+use titan_shared::execution::trackers::tracker::Tracker;
+use WhichRegister::Pc;
+
+impl RegisterEntry {
+    pub fn apply<Reg: Registers>(self, registers: &mut Reg) {
+        let RegisterEntry(name, value) = self;
+        registers.set(name, value);
+    }
+}
+
+pub struct HistoryEntry {
+    pub registers: SmallVec<[RegisterEntry; REGISTER_LOG_SIZE]>,
+    pub edits: SmallVec<[WatchEntry; LOG_SIZE]>,
+}
+
+impl HistoryEntry {
+    pub fn apply<Mem: Memory, Reg: Registers>(self, registers: &mut Reg, memory: &mut Mem) {
+        // For RISC-V, the processor order has changed compared to the MIPS Titan Core.
+        // So we sub PC first here, and this will be overwritten by the registers later.
+        registers.set(Pc, registers.get(Pc).wrapping_sub(4));
+
+        for entry in self.registers.iter().rev() {
+            entry.apply(registers);
+        }
+
+        for entry in self.edits {
+            entry.apply(memory).ok(); // ignore error
+        }
+    }
+}
+
+pub struct HistoryTracker {
+    buffer: VecDeque<HistoryEntry>,
+}
+
+impl HistoryTracker {
+    pub fn new(capacity: usize) -> HistoryTracker {
+        HistoryTracker {
+            buffer: VecDeque::with_capacity(capacity),
+        }
+    }
+
+    fn push(&mut self, entry: HistoryEntry) {
+        if self.buffer.capacity() == self.buffer.len() {
+            self.buffer.pop_front();
+        }
+        self.buffer.push_back(entry);
+    }
+
+    pub fn pop(&mut self) -> Option<HistoryEntry> {
+        self.buffer.pop_back()
+    }
+
+    pub fn last(&mut self) -> Option<&HistoryEntry> {
+        self.buffer.back()
+    }
+
+    pub fn len(&self) -> usize {
+        self.buffer.len()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.buffer.is_empty()
+    }
+}
+
+impl<Mem: Memory> Tracker<State<WatchedMemory<Mem>, WatchedRegisters>> for HistoryTracker {
+    fn pre_track(&mut self, _state: &mut State<WatchedMemory<Mem>, WatchedRegisters>) {}
+
+    fn post_track(&mut self, state: &mut State<WatchedMemory<Mem>, WatchedRegisters>) {
+        let entry = HistoryEntry {
+            registers: state.registers.take(),
+            edits: state.memory.take(),
+        };
+
+        self.push(entry);
+    }
+}
